f3525dc5a05e1dddb7a5e64454a9f21506b1e076
Luis Gonzalez <l.adrian877@gmail.com>
2026-06-18 16:04:07 +0200

    Style changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  safelist: ['col-span-2', 'col-span-4'],
+  safelist: [
+    'col-span-1','col-span-2','col-span-4',
+    'sm:col-span-2','lg:col-span-2','lg:col-span-4',
+  ],
   theme: {
     extend: {
       fontFamily: {
